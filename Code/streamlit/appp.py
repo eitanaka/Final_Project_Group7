300bcc3cec9a01626f24f8b7d24a136b62639ba1
swarnachandran <74671975+swarnachandran@users.noreply.github.com>
2023-12-10 21:10:49 -0500

    Update appp.py

--- Code/streamlit/appp.py
+++ Code/streamlit/appp.py
@@ -12,11 +12,12 @@ import streamlit as st
 from st_pages import Page, show_pages, add_page_title
 
 # ============================== Setup / constants ====================================
-# Set up paths
 OS_PATH = os.getcwd()
 os.chdir("../../")
 ROOT_PATH = os.getcwd()
 IMAGE_PATH = os.path.join(OS_PATH, 'assets')
+IMAGE_FILENAME = 'FIGURE01.png'  # Adjust the filename as needed
+IMAGE_PATH1 = os.path.join(IMAGE_PATH, IMAGE_FILENAME)
 MODEL_PATH = os.path.join(ROOT_PATH, "Models")
 ELECTRA_PATH = os.path.join(MODEL_PATH, "ELECTRA", "electra-finetuned-squadv2")
 XLNET_PATH = ""
