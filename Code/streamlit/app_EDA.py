d41a564d415a6734fa9c6791701e53ca9a561591
swarnachandran <74671975+swarnachandran@users.noreply.github.com>
2023-12-11 00:26:52 -0500

    Add files via upload

--- Code/streamlit/app_EDA.py
+++ Code/streamlit/app_EDA.py
@@ -8,6 +8,7 @@ from st_pages import Page, show_pages, add_page_title
 OS_PATH = os.getcwd()
 os.chdir("../../")
 ROOT_PATH = os.getcwd()
+IMAGE_PATH = os.path.join(OS_PATH, 'assets')
 MODEL_PATH = os.path.join(ROOT_PATH, "Models")
 ELECTRA_PATH = os.path.join(MODEL_PATH, "ELECTRA", "electra-finetuned-squadv2")
 XLNET_PATH = ""
@@ -25,3 +26,11 @@ st.title("Question Answering on SQuAD 2.0")
 st.header("Exploratory Data Analysis (EDA)")
 
 # Add contents for the page here.
+genre = st.radio(
+        "What you wanna explore",
+        ["*Frequency of answers*", "Length analysis", "Answerable vs Unanswerable"],
+        captions = ["Know it", "measure it", "question it"],
+        index=None,
+    )
+
+st.write("You selected:", genre)
